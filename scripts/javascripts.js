3c6defab18da40489cbfebf9df4d1595e2d1db35
Aaron <aaronlhma@gmail.com>
2020-11-24 16:19:21 -0800

    Adds function: createCourseArray from courseList

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -3,6 +3,25 @@ courseList = [{code: "ACIT 1620", name: "Web Tech"},
 {code: "ACIT 1515", name: "Python"},
 {code: "ACIT 1420", name: "System Admins"}];
 
+function createCourseArray(courseList){
+    let courseArray = [];
+    for(i=0; i<courseList.length;i++){
+        courseName = courseList[i].code +' ' + courseList[i].name;
+        courseObj = {code: courseName, date: "Fall 2020"};
+        courseArray[i] = courseObj;
+    } 
+    return courseArray;
+}
+
+function findCourse(courseList){
+    do {
+        var code = prompt("Enter a 4-digit course code: ");
+    } while ( (isNaN(code) == true) || (code.length != 4))
+    
+}
+
+courseList = createCourseArray(courseList);
+
 do {
     var code = prompt("Enter a 4-digit course code: ");
 } while ( (isNaN(code) == true) || (code.length != 4))
@@ -11,7 +30,7 @@ courseExists = false;
 
 for (i=0;i<courseList.length;i++){
     if (courseList[i].code.includes(code)){
-        console.log(`Yes I am taking the course: ${courseList[i].code} ${courseList[i].name}`);
+        console.log(`Yes I am taking the course: ${courseList[i].code}`);
         courseExists = true;
         break;
     }
